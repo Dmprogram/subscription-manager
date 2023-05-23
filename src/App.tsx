ef5802cb15b45e21e317418539acbc36b4594548
Dmitriy <dmprogramhh@mail.ru>
2023-05-23 15:10:24 +0300

    add-navigation-to-login

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ const App = () => {
             <Route path='/edit-subscription/:subscriptionId' element={<EditSubscriptionPage />} />
             <Route path='*' element={<Navigate to='/' replace />} />
           </Route>
+          <Route path='*' element={<Navigate to='/login' replace />} />
         </Routes>
       </BrowserRouter>
     </div>
